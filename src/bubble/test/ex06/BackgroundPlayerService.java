7616af6f4a7a20c31c1ef9bfa1bd56202b8b5358
kyeonghooon <gibbon0510@gmail.com>
2024-05-03 12:08:55 +0900

    6단계완성

---
+++ src/bubble/test/ex06/BackgroundPlayerService.java
@@ -0,0 +1,78 @@
+package bubble.test.ex06;
+
+import java.awt.Color;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+/**
+ * 현재 메인 쓰레드는 너~무 바쁨
+ * 백그라운드에서 계속 Player의 움직임을 관찰할 예정
+ */
+public class BackgroundPlayerService implements Runnable {
+
+	private BufferedImage image;
+	private Player player;
+	
+	// 생성자 의존 주입 DI
+	public BackgroundPlayerService(Player player) {
+		this.player = player;
+		
+		try {
+			image = ImageIO.read(new File("img/backgroundMapService.png"));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	@Override
+	public void run() {
+		while(true) {
+			System.out.println("1111");
+			// 색상 확인 todo (보정값 필요)
+			Color leftColor = new Color(image.getRGB(player.getX() + 10, player.getY() + 25));
+			Color rightColor = new Color(image.getRGB(player.getX() + 50 + 10, player.getY() + 25));
+			// Color bottomColor = new Color(image.getRGB(player.getX(), player.getY()));
+			
+			// 흰색이면  RGB => 255 255 255
+			// 빨간바닥인 경우 --> 255 0 0 
+			// 파란바닥인 경우 --> 0 0 255
+			int bottomColorLeft = image.getRGB(player.getX() + 20, player.getY() + 50 + 5);
+			int bottomColorRight = image.getRGB(player.getX() + 50 + 10, player.getY() + 50 + 5);
+			
+			// 흰색이면 -----> int 값이 - 2
+			if(bottomColorLeft + bottomColorRight != -2) {
+				// 여기는 멈추어야 한다. (빨간 바닥 또는 파란 바닥)
+				player.setDown(false);
+				// 플레이어가 올라가는 상태가 아니고, 내려가는 상태가 아니면 down() 호출
+			} else if (!player.isUp() && !player.isDown()) {
+					player.down();
+			}
+			System.out.println("bottomColor : " + bottomColorLeft);
+			
+			// 왼쪽 벽에 충돌함
+			if (leftColor.getRed() == 255 && leftColor.getGreen() == 0 && leftColor.getBlue() == 0) {
+				System.out.println("왼쪽벽에 충돌함");
+				player.setLeftWallCrash(true);
+				player.setLeft(false);
+			} else if (rightColor.getRed() == 255 && rightColor.getGreen() == 0 && rightColor.getBlue() == 0) {
+				System.out.println("오른쪽벽에 충돌함");
+				player.setRightWallCrash(true);
+				player.setRight(false);
+			} else {
+				player.setLeftWallCrash(false);
+				player.setRightWallCrash(false);
+			}
+			// 위 두 조건이 아니면 player는 마음대로 움직일 수 있다.
+			
+			try {
+				Thread.sleep(10);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+}
---
+++ src/bubble/test/ex06/BubbleFrame.java
@@ -0,0 +1,103 @@
+package bubble.test.ex06;
+
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+public class BubbleFrame extends JFrame {
+
+	private JLabel backgroundMap;
+	private Player player;
+	private final int BG_WIDTH = 1000;
+	private final int BG_HEIGHT = 640;
+
+	public BubbleFrame() {
+		initData();
+		setInitLayout();
+		addEventListener();
+
+		// Player 백그라운드 서비스 시작
+
+		new Thread(new BackgroundPlayerService(player)).start();
+	}
+
+	private void initData() {
+		// todo 이미지 변경
+		backgroundMap = new JLabel(
+				new ImageIcon("img/backgroundMap.png"));
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		// Frame -> root Panel
+		setContentPane(backgroundMap); // add 처리
+		setSize(BG_WIDTH, BG_HEIGHT);
+
+		player = new Player();
+	}
+
+	private void setInitLayout() {
+		setLayout(null); // 좌표값으로 배치
+		setResizable(false); // 프레임 조절 불가
+		setLocationRelativeTo(null); // JFrame을 모니터 가운데 자동 배치
+		setVisible(true);
+
+		add(player);
+	}
+
+	private void addEventListener() {
+		// KeyAdapter는 KeyListener 를 구현한 추상 클래스
+		// 불필요한 메서드를 쓰지 않고 원하는 메서드만 오버라이드해서 쓰면됨
+		addKeyListener(new KeyAdapter() {
+			@Override
+			public void keyPressed(KeyEvent e) {
+				// 개발단계에서는 확인 용도로 써놓음
+				System.out.println("key code : " + e.getKeyCode());
+
+				switch (e.getKeyCode()) {
+					case KeyEvent.VK_LEFT :
+
+						// 왼쪽 상태가 아니라면
+						// 왼쪽 벽에 충돌한게 아니라면
+						if (!player.isLeft() && !player.isLeftWallCrash()) {
+							player.left();
+						}
+						break;
+					case KeyEvent.VK_RIGHT :
+						if (!player.isRight() && !player.isRightWallCrash()) {
+							player.right();
+						}
+						break;
+					case KeyEvent.VK_UP :
+						player.up();
+						break;
+					case KeyEvent.VK_SPACE :
+						add(new Bubble(player));
+					default :
+						break;
+				}
+			} // end of KeyPressed
+			@Override
+			public void keyReleased(KeyEvent e) {
+				switch (e.getKeyCode()) {
+					case KeyEvent.VK_LEFT :
+						player.setLeft(false);
+						break;
+					case KeyEvent.VK_RIGHT :
+						player.setRight(false);
+						break;
+					default :
+						break;
+				}
+
+			} // end of KeyReleased
+		});
+	}
+
+	// 코드 테스트
+	public static void main(String[] args) {
+		new BubbleFrame();
+	} // end of main
+
+}
